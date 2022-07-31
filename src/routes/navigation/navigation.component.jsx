ca87fa2d2435d3a789b6e7f774e9f68c087e7cdd
Yevgeniy Ivanov <yev@Yevgeniys-MBP.home>
2022-07-31 16:14:37 -0400

    implement basic navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,27 @@
 import './navigation.styles.scss';
 
+import { Outlet } from 'react-router-dom';
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return(
+      <>
         <div className='navigation-container'>
-            <h1>NAVIGATION</h1>
+            <div className='navigation-section logo-section'>
+                LOGO
+            </div>
+            <div className='navigation-section links-section'>
+                <span className='home-link navigation-link'>HOME</span>
+                <span className='browse-link navigation-link'>BROWSE</span>
+                <span className='about-link navigation-link'>ABOUT</span>
+                <span className='contact-link navigation-link'>CONTACT</span>
+            </div>
+            <div className='navigation-section media-links-section'>
+                HELLO
+            </div>
         </div>
+        <Outlet />
+      </>
     )
 }
 
